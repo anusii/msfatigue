587c8d0283acb62238828dcd7dbb182d13b337b6
zheyxu <79696631+zheyxu@users.noreply.github.com>
2024-08-25 05:02:57 +1000

    add dart_code_metrics

--- lib/utils/rdf.dart
+++ lib/utils/rdf.dart
@@ -23,8 +23,6 @@
 ///
 /// Authors: Dawei Chen
 
-// TODO 20240708 gjw CAN ANY OF THIS GO TO RDFLIB?
-
 library;
 
 import 'package:rdflib/rdflib.dart';
@@ -67,11 +65,7 @@ Future<String> genTTLStr(
 /// Predicate: Key
 /// Object: Value
 
-// TODO 20240710 gjw COULD THIS USE rdflib::parseTTL
-
 Future<List<({String key, dynamic value})>> parseTTLStr(String ttlStr) async {
-  // TODO 20240710 gjw COMMENT THAT WE COULD DO WITH USING MORE asserts()
-
   assert(ttlStr.isNotEmpty);
   final g = Graph();
   g.parseTurtle(ttlStr);
