ccb182dc1f611e174c581f711bfa54d7c0a2c4f8
zheyxu <79696631+zheyxu@users.noreply.github.com>
2024-09-06 15:01:54 +1000

    fix popup issue

--- lib/widgets/dialog/show_file_content.dart
+++ lib/widgets/dialog/show_file_content.dart
@@ -20,71 +20,74 @@ void showFileContent(
     String content = await readPod(filePath, context, Container());
     Map questionsList = await listSurveyQuestions(content);
 
-    showDialog(
-      // ignore: use_build_context_synchronously
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: Text(fileName),
-          content: SingleChildScrollView(
-            child: Table(
-              columnWidths: const {
-                0: FlexColumnWidth(2), // Question column width
-                1: FlexColumnWidth(1), // Answer column width
-              },
-              border:
-                  TableBorder.all(color: Colors.grey), // Add borders to table
-              children: [
-                // Header Row
-                TableRow(
-                  decoration: BoxDecoration(color: Colors.grey[200]),
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Question',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Answer',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-                // Data Rows.
-                for (var entry in questionsList.entries)
+    if (questionsList.isNotEmpty) {
+      showDialog(
+        // ignore: use_build_context_synchronously
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(fileName),
+            content: SingleChildScrollView(
+              child: Table(
+                columnWidths: const {
+                  0: FlexColumnWidth(2), // Question column width
+                  1: FlexColumnWidth(1), // Answer column width
+                },
+                border:
+                    TableBorder.all(color: Colors.grey), // Add borders to table
+                children: [
+                  // Header Row
                   TableRow(
-                    children: [
+                    decoration: BoxDecoration(color: Colors.grey[200]),
+                    children: const [
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: EdgeInsets.all(8.0),
                         child: Text(
-                          cleanNumberDot(entry.key),
+                          'Question',
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: EdgeInsets.all(8.0),
                         child: Text(
-                          cleanNumberDot(entry.value),
+                          'Answer',
+                          style: TextStyle(fontWeight: FontWeight.bold),
                         ),
                       ),
                     ],
                   ),
-              ],
+                  // Data Rows.
+                  for (var entry in questionsList.entries)
+                    TableRow(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            cleanNumberDot(entry.key),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            cleanNumberDot(entry.value),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              ),
             ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Close'),
-            ),
-          ],
-        );
-      },
-    );
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('Close'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     // ignore: empty_catches
   } catch (e) {}
 }
