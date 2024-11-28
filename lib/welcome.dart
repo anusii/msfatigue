e03003f12ec9b40d2b5f4046843d538cdce82ea6
zheyxu <79696631+zheyxu@users.noreply.github.com>
2024-11-28 16:43:22 +1100

    update

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -55,81 +55,93 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           ],
         ),
       ),
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 10),
-                const Text(
-                  'Welcome Jenny!',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.pink,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  'You\'ve been invited to participate in a fatigue survey for people with multiple sclerosis (MS).',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  'The focus of the survey is your recent experiences of fatigue with a focus on how you are feeling \'right now\'.',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 8),
-                const Text(
-                  'The survey should take 10-20 minutes to complete.',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 30),
-                Center(
-                  child: SizedBox(
-                    width: 260,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ConsentScreen()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        side: const BorderSide(color: Colors.pink),
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: const Text(
-                        'Take me to the survey ►',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.black,
-                        ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          const SizedBox(height: 10),
+                          const Text(
+                            'Welcome Jenny!',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.pink,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                          const Text(
+                            'You\'ve been invited to participate in a fatigue survey for people with multiple sclerosis (MS).',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text(
+                            'The focus of the survey is your recent experiences of fatigue with a focus on how you are feeling \'right now\'.',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text(
+                            'The survey should take 10-20 minutes to complete.',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(height: 30),
+                          Center(
+                            child: SizedBox(
+                              width: 260,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ConsentScreen()),
+                                  );
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  side: const BorderSide(color: Colors.pink),
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 16),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                ),
+                                child: const Text(
+                                  'Take me to the survey ►',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
+                    // Bottom dot image.
+                    Image.asset(
+                      'assets/images/bottom_dot_one.png',
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 32),
-              ],
+              ),
             ),
-          ),
-          Positioned(
-            bottom: 0, 
-            left: 0,
-            right: 0,
-            child: Image.asset(
-              'assets/images/bottom_dot.png',
-              fit: BoxFit.contain,
-            ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
